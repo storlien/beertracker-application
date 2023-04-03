5aa5e630231769d7199856e3f107966659344e57
Edward Storlien <carlst@stud.ntnu.no>
2023-04-03 11:24:10 +0200

    Ferdig med Filehandler

--- src/main/java/com/carlst/Filehandler.java
+++ src/main/java/com/carlst/Filehandler.java
@@ -5,22 +5,48 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Filehandler {
-    List<Person> objPersons = new ArrayList<>();
 
-    public void readFile() {
+    public List<Person> readFile() {
+        List<Person> objPersons = new ArrayList<>();
 
         try {
             BufferedReader bf = new BufferedReader(new FileReader("src/main/resources/json/persons.txt"));
 
+            // Hver linje representerer ett Person-objekt
+            int index = 0;
             String line;
+            String[] lineList;
             while ((line = bf.readLine()) != null) {
-                System.out.println(line);
+                lineList = line.split("-");
+                
+                // Instansierer Person-objekt
+                objPersons.add(new Person(lineList[0], lineList[1]));
+                
+                // Legger til summen
+                double amountSpent = Double.parseDouble(lineList[2]);
+                objPersons.get(index).depositAmount(amountSpent);
+
+                List<String> cardsList = new ArrayList<>(Arrays.asList(lineList));
+                cardsList = cardsList.subList(3, cardsList.size());
+
+                for (String card : cardsList) {
+                    int firstSix = Integer.parseInt(card.substring(0, 6));
+                    int lastFour = Integer.parseInt(card.substring(6));
+
+                    // Legger til kortet
+                    objPersons.get(index).addCard(firstSix, lastFour);
+                }
+
+                index++;
             }
 
             bf.close();
+
+            return objPersons;
         }
 
         catch (Exception e) {
@@ -35,14 +61,13 @@ public class Filehandler {
             String line;
 
             for (Person person : objPersons) {
-                line = person.getFirstName() + "-" + person.getLastName();
+                line = person.getFirstName() + "-" + person.getLastName() + "-" + person.getAmountSpent();
 
+                // Legger til alle kort på linjen
                 line += person.getCards().stream().reduce("", (acc, c) -> {
                     acc += "-" + c;
                     return acc;
                 });
-
-                System.out.println(line);
 
                 // Hvis personen ikke er siste i for-løkken (ikke siste element av listen)
                 if (!(objPersons.indexOf(person) == objPersons.size() - 1)) {
@@ -64,20 +89,30 @@ public class Filehandler {
         Filehandler fh = new Filehandler();
 
         Person edward = new Person("Edward", "Storlien", 123456, 7890);
+        edward.depositAmount(100.5);
         edward.addCard(696969, 6969);
         edward.addCard(989898, 4545);
 
         Person mamma = new Person("Mamma", "Storlien", 123456, 7890);
+        mamma.depositAmount(16.7);
         mamma.addCard(990023, 9200);
 
         Person fnedd = new Person("Fnedd", "Storlien", 123456, 7890);
+        fnedd.depositAmount(69.69);
         fnedd.addCard(666666, 6666);
+
+        Person louise = new Person("Louise", "Storlien");
 
         List<Person> personer = new ArrayList<>();
         personer.add(edward);
         personer.add(mamma);
         personer.add(fnedd);
+        personer.add(louise);
 
         fh.saveFile(personer);
+
+        System.out.println("Ferdig med lagring av fil.");
+
+        fh.readFile();
     }
 }
